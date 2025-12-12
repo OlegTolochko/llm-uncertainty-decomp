a6297b146cca90b1657cd468e8f803b2426021a5
oleg <oleg.i.tolochko@gmail.com>
2025-12-12 14:03:25 +0100

    Update inference code to utilize OpenAI SDK

---
+++ llm_inference.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1", api_key=os.getenv("OPENROUTER_API_KEY")
+)
+
+
+def inference(model_url: str, content: str):
+    response = client.chat.completions.create(
+        extra_headers={
+            "HTTP-Referer": "<YOUR_SITE_URL>",
+            "X-Title": "<YOUR_SITE_NAME>",
+        },
+        model=model_url,
+        messages=[{"role": "user", "content": content}],
+    )
+    return response
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import cyclopts
 
+from llm_inference import inference
+
 app = cyclopts.App()
 
 
 @app.command()
-def main():
-    print("Hello from llm-uncertainty-decomp!")
+def main(
+    model_url: str = "deepseek/deepseek-v3.2",
+    content: str = "how many r's are in the word strawberry?",
+):
+    response = inference(model_url=model_url, content=content)
+    print(response)
 
 
 if __name__ == "__main__":
